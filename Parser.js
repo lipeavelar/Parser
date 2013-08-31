d01acc71134e53ea1b71fed071a6de37d63a3500
Felipe Avelar <lipeavelar@gmail.com>
2013-08-31 20:19:37 -0300

    v1.1

--- Parser.js
+++ Parser.js
@@ -4,6 +4,23 @@ function Parser(exp){
 	var expression;
 	var iElement;
 	
+	//Constants
+	var CONSTANTS ={};
+	CONSTANTS["pi"] = Math.PI;
+	CONSTANTS["e"] = Math.E;
+	var RESERVED_NAMES = new Array	(	//Operations
+										"sin",
+										"cos",
+										"tan",
+										"log",
+										"min",
+										"max",
+										"abs",
+										//constants
+										"pi",
+										"e"
+									)
+	
 	Parser(exp);
 	/**
 	 *Creates the object with the expression to be evaluated
@@ -30,6 +47,9 @@ function Parser(exp){
 		}
 		if(!__verifyVariable(variable)){
 			throw "Invalid characters in variable name.";
+		}
+		if(__isReservedName(variable)){
+			throw "This name is a function or a constant.";
 		}
 		if(typeof(value) == "number"){
 			throw "Invalid value.";
@@ -90,13 +110,27 @@ function Parser(exp){
 	}
 	
 	/**
+	 *Verify if this name is used for some operations
+	 *@param	name	The name to be verified. 
+	*/
+	function __isReservedName(name){
+		return RESERVED_NAMES.indexOf(name) > -1
+	}
+	
+	/**
 	 *Replace variables with their respective values
 	*/
 	function __replaceValues(){
 		var result = expression;
+		//substitute variable values
 		for(var key in variableValues){
 			var regex = new RegExp("\\b"+key+"\\b", "g");
 			result = result.replace(regex, variableValues[key]);
+		}
+		//substitute constant values
+		for(var key in CONSTANTS){
+			var regex = new RegExp("\\b"+key+"\\b", "g");
+			result = result.replace(regex, CONSTANTS[key]);
 		}
 		return result;
 	}
@@ -112,8 +146,8 @@ function Parser(exp){
 	//PARSER RULES
 	/*
 		number     	= {"0"|"1"|"2"|"3"|"4"|"5"|"6"|"7"|"8"|"9"|"."|" "}
-		factor  	= number | "(" expression ")"
-		element 	= factor [{("^")} factor]
+		factor  	= number | "(" expression ")" | wordInFunctionsArray"(" factor ["," factor] ")"
+		element 	= factor [{"^" factor}]
 		component  	= element [{("*" | "/") element}]
 		expression 	= component [{("+" | "-") component}]
 	*/
@@ -124,10 +158,10 @@ function Parser(exp){
 			if(exp[iElement] == "+"){
 				iElement++;
 				result += __component(exp);
-			}else if(exp[iElement] == "+"){
+			}else if(exp[iElement] == "-"){
 				iElement++;
 				result -= __component(exp);
-			}else if(exp[iElement] == ")"){
+			}else if(exp[iElement] == ")" || exp[iElement] == ","){
 				break;
 			}
 		}
@@ -159,11 +193,47 @@ function Parser(exp){
 	
 	function __factor(exp){
 		var result = 0;
+		var functions = new Array("sin", "cos", "tan","log","max","min","abs");
 		if(exp[iElement] == "("){
 			iElement++;
 			result = __expression(exp);
 			if(exp[iElement] != ")") throw "Missing ')' in column "+iElement.toString()+".";
 			iElement++;
+		}else if(functions.indexOf(exp[iElement]+exp[iElement+1]+exp[iElement+2]) > -1 && exp[iElement+3] == "("){
+			//function verify
+			var f = exp[iElement]+exp[iElement+1]+exp[iElement+2];
+			iElement += 4;
+			var num1, num2;
+			var verifyNum2 = false;
+			num1 = __expression(exp);
+			if(exp[iElement] == ","){
+				if(parseFloat(num1) != parseFloat(num1)) throw "Expected a number in column "+iElement-1+".";
+				iElement++;
+				verifyNum2 = true;
+				num2 = __expression(exp);
+			}
+			if(exp[iElement] != ")") throw "Missing ')' in column "+iElement.toString()+".";
+			iElement++;
+			if(verifyNum2){
+				if(parseFloat(num2) != parseFloat(num2)) throw "Expected a number in column "+iElement-2+".";
+			}else{
+				if(parseFloat(num1) != parseFloat(num1)) throw "Expected a number in column "+iElement-2+".";
+			}
+			if(f == "sin"){
+				result = Math.sin(num1);
+			}else if(f == "cos"){
+				result = Math.cos(num1);
+			}else if(f == "tan"){
+				result = Math.tan(num1);
+			}else if(f == "log"){
+				result = Math.log(num1)/Math.log(num2);
+			}else if(f == "min"){
+				result = Math.min(num1,num2);
+			}else if(f == "max"){
+				result = Math.max(num1,num2);
+			}else if(f == "abs"){
+				result = Math.abs(num1);
+			}
 		}else {
 			result = __number(exp);
 		}
